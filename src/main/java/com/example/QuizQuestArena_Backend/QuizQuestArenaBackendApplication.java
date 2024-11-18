53587897bdd569a3adbb84dc295715c81a9c1846
nvgrowold <nvgrowold@gmail.com>
2024-11-19 09:05:16 +1300

    Initial commit

---
+++ src/main/java/com/example/QuizQuestArena_Backend/QuizQuestArenaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizQuestArena_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizQuestArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizQuestArenaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuizQuestArena_Backend/QuizQuestArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuizQuestArena_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizQuestArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
