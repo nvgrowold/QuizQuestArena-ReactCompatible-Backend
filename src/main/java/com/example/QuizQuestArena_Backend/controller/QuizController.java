04602266915a66b9f5ed5a810f8b8e22d584b40f
nvgrowold <nvgrowold@gmail.com>
2024-11-29 11:04:38 +1300

    Bug fixing: Quiz complete - likes woring

--- src/main/java/com/example/QuizQuestArena_Backend/controller/QuizController.java
+++ src/main/java/com/example/QuizQuestArena_Backend/controller/QuizController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
